5534900dd8217fb59c1e6ae5d09d5e6f65f06dc5
seungmoo.lee <seungmoo.lee@lotte.net>
2021-02-04 22:54:32 +0900

    Class 기반 프록시 생성하기

--- src/test/java/com/seungmoo/thejavaproxy/BookServiceTest.java
+++ src/test/java/com/seungmoo/thejavaproxy/BookServiceTest.java
@@ -1,10 +1,19 @@
 package com.seungmoo.thejavaproxy;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.InvocationHandlerAdapter;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+
+import static net.bytebuddy.matcher.ElementMatchers.named;
 
 class BookServiceTest {
 
@@ -48,4 +57,62 @@ class BookServiceTest {
         book.setTitle("spring");
         bookService.rent(book);
     }
+
+    /**
+     * CGlib, ByteBuddy 모두 상속을 통한  subClass를 만드는 방법으로 프록시 객체를 생성한다.
+     * final class 는 상속을 못받기 때문에 불가하다.
+     * Default Constructor를 private 하게 정의했을 경우 또한 불가한다. (하위 클래스는 부모 클래스의 생성자를 호출)
+     * --> 이런 이유로 웬만하면 Interface로 Proxy를 생성하는게 좋다.
+     */
+
+    @Test
+    @DisplayName("CGlib을 이용한 Class 기반의 Proxy 만들기")
+    void classProxy() {
+        MethodInterceptor handler = new MethodInterceptor() {
+            DefaultBookService bookService = new DefaultBookService();
+            @Override
+            public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+                if (method.getName().equals("rent")) {
+                    System.out.println("aaaa");
+                    Object invoke =  method.invoke(bookService, args);
+                    System.out.println("bbbb");
+                    return invoke;
+                }
+                return method.invoke(bookService, args);
+            }
+        };
+
+        // Enhancer : CGlib의 핵심적인 Class
+        DefaultBookService bookService = (DefaultBookService) Enhancer.create(DefaultBookService.class, handler);
+
+        Book book = new Book();
+        book.setTitle("spring");
+        bookService.rent(book);
+        bookService.returnBook(book);
+    }
+
+    @Test
+    @DisplayName("ByteBuddy를 이용한 Class 기반의 Proxy 만들기")
+    void classProxy2() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        // Proxy Class 정의
+        Class<? extends DefaultBookService> proxyClass = new ByteBuddy().subclass(DefaultBookService.class)
+                .method(named("rent")).intercept(InvocationHandlerAdapter.of(new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        System.out.println("aaaa");
+                        Object invoke = method.invoke(bookService, args);
+                        System.out.println("bbbb");
+                        return invoke;
+                    }
+                }))
+                .make().load(DefaultBookService.class.getClassLoader()).getLoaded();
+        // 객체 인스턴스 생성
+        DefaultBookService bookService = proxyClass.getConstructor(null).newInstance();
+
+        Book book = new Book();
+        book.setTitle("spring");
+        bookService.rent(book);
+        bookService.returnBook(book);
+    }
+
 }
